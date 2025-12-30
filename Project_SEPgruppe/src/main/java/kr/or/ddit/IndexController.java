cc120a028e89db07e970f6651defd64b257b7d7b
sm-studiocode <sm.codes.coding@gmail.com>
2025-12-30 19:32:28 +0900

    front commit--one

--- Project_SEPgruppe/src/main/java/kr/or/ddit/IndexController.java
+++ Project_SEPgruppe/src/main/java/kr/or/ddit/IndexController.java
@@ -1,5 +1,19 @@
 package kr.or.ddit;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+	
+	@GetMapping("/")
+	public String sepIndex() {
+		return "sep:indexSep";
+	}
+	
+	@GetMapping("/groupware")
+	public String groupwareIndex() {
+		return "gw:indexGW";
+	}
 
 }
---
+++ Project_SEPgruppe/src/main/java/kr/or/ddit/security/CustomUserDetailService.java
@@ -0,0 +1,55 @@
+package kr.or.ddit.security;
+
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import kr.or.ddit.works.login.vo.AllUserVO;
+import kr.or.ddit.works.mybatis.mappers.LoginMapper;
+import lombok.extern.slf4j.Slf4j;
+
+//@Component
+//@Slf4j
+//public class CustomUserDetailService implements UserDetailsService{
+//	
+//	@Autowired
+//	private LoginMapper mapper;
+//	
+//	@Override
+//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//		
+//		// 아이디로 사용자 조회
+//		AllUserVO user = mapper.login(username);
+//		
+//		// 사용자가 없을 경우 예외 발생
+//		if(user==null) throw new UsernameNotFoundException(String.format("%s 사용자 없음.", username));
+//		
+//		// 로그 확인
+//		log.info("🔍 로그인 시도 - ID: {}", username);
+//
+//		// UserDetails 구현체로 반환
+//		return new RealUserWrapper(user);
+//	}
+//
+//}
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
